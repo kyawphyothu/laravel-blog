14e5a533ea87f775a2b5608aa7f11c37b05b9d7d
Kyaw Phyo Thu <kyawphyothukpt256@gmail.com>
2022-04-03 10:51:22 +0630

    Model

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,15 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $data = [
-            ["name"=>"zaw", "title"=>"one"],
-            ["name"=>"kyaw", "title"=>"two"],
-        ];
+        $data = Article::all();
 
         return view('articles.index', [
             'articles' => $data
