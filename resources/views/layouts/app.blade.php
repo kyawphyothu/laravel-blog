988aa874d7a295bb835ff734d3fa2f980f4afeac
sudoMan <kyawphyothukpt256@gmail.com>
2022-04-23 09:35:59 +0630

    + Add Article feature

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a href="{{ url('/articles/add') }}" class="nav-link text-success">+ Add Article</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
