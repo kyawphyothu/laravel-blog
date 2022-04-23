55a5a180a091bf53c082680ca3ed2ed2a3112a71
sudoMan <kyawphyothukpt256@gmail.com>
2022-04-23 16:49:52 +0630

    page 717

--- resources/views/articles/detail.blade.php
+++ resources/views/articles/detail.blade.php
@@ -7,6 +7,7 @@
                 <h5 class="card-title">{{ $article->title }}</h5>
                 <div class="card-subtitle mb-2 text-muted small">
                     {{ $article->created_at->diffForHumans() }}
+                    Category: <b>{{ $article->category->name }}</b>
                 </div>
                 <p class="card-text">
                     {{ $article->body }}
@@ -14,5 +15,16 @@
                 <a href="{{ url("/articles/delete/$article->id") }}" class="btn btn-warning">Delete</a>
             </div>
         </div>
+
+        <ul class="list-group">
+            <li class="list-group-item active">
+                <b>Comments ({{ count($article->comments) }})</b>
+            </li>
+            @foreach ($article->comments as $comment)
+                <li class="list-group-item">
+                    {{ $comment->count }}
+                </li>
+            @endforeach
+        </ul>
     </div>
 @endsection
