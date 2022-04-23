c681f237070601110e05003bdfdfe41e2e31b1de
sudoMan <kyawphyothukpt256@gmail.com>
2022-04-23 12:18:27 +0630

    laravel form finish

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -22,4 +22,45 @@ class ArticleController extends Controller
 
         return view('articles.detail', ['article' => $article]);
     }
+
+    public function add()
+    {
+        $data = [
+            ["id"=>1, "name"=> "News"],
+            ["id"=>2, "name"=> "Tech"],
+        ];
+
+        return view('articles.app', [
+            'categories' => $data
+        ]);
+    }
+
+    public function create()
+    {
+        $validator = validator(request()->all(), [
+            'title' => 'required',
+            'body' => 'required',
+            'category_id' => 'required',
+        ]);
+
+        if($validator->fails()) {
+            return back()->withErrors($validator);
+        }
+
+        $article = new Article;
+        $article->title = request()->title;
+        $article->body = request()->body;
+        $article->category_id = request()->category_id;
+        $article->save();
+
+        return redirect('/articles');
+    }
+
+    public function delete($id)
+    {
+        $article = Article::find($id);
+        $article->delete();
+
+        return redirect('/articles')->with('info', 'Article deleted');
+    }
 }
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <div class="container">
+
+        @if (session('info'))
+            <div class="alert alert-info">
+                {{ session('info') }}
+            </div>
+        @endif
+
         {{ $articles->links() }}
         @foreach ($articles as $article)
         <div class="card mb-2">
