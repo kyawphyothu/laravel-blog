8bd8565e291751cdd8a859f4c915a2267c620d9b
sudoMan <kyawphyothukpt256@gmail.com>
2022-04-24 01:31:11 +0630

    Laravel Model Relationship

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,18 +15,27 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Article Routes
 Route::get('/', [ArticleController::class, "index"]);
 Route::get('/articles', [ArticleController::class, "index"]);
 
 Route::get('articles/detail/{id}', [ArticleController::class, "detail"]);
+
+Route::get('/articles/add', [ArticleController::class, 'add']);
+Route::post('/articles/add', [ArticleController::class, 'create']);
+
+Route::get('/articles/delete/{id}', [ArticleController::class, 'delete']);
+
+//////////////////////////////
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::get('/articles/add', [ArticleController::class, 'add']);
-Route::post('/articles/add', [ArticleController::class, 'create']);
+//Comment Route
+Route::post('/comment/add', [CommentController::class, 'add']);
+Route::get('/comment/delete/{id}', [CommentController::class, 'delete']);
 
-Route::get('/articles/delete/{id}', [ArticleController::class, 'delete']);
+
+
